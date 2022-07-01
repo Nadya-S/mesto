40d225ad9b8a6d8d0d626f44e337136382ddace0
Nadya Sadovskaya <di.menson@yandex.ru>
2022-07-01 18:23:13 +0400

    feat: make add form

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,19 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__button-close');
-let popup = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__input_type_name');
-let aboutInput = document.querySelector('.popup__input_type_about');
+const editProfileModal = document.querySelector('.edit-profile-popup');
+const addPlaceModal = document.querySelector('.add-place-popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButtonPlace = addPlaceModal.querySelector('.popup__button-close');
+const closeButtonProfile = editProfileModal.querySelector('.popup__button-close');
+const addButton = document.querySelector('.profile__add-button');
+const popup = document.querySelector('.popup');
+const nameInput = document.querySelector('.popup__input_type_name');
+const aboutInput = document.querySelector('.popup__input_type_about');
 let formElement = document.querySelector('.popup__container');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
 const placesList = document.querySelector('.places__list');
 const placeTemplate = document.querySelector('.place-template').content;
+const saveCardButton = document.querySelector('#button-save-card');
+const saveProfileButton = document.querySelector('#button-save-profile');
 
 const initialPlaces = [
     {
@@ -35,39 +41,92 @@ const initialPlaces = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
-
+// загрузка карточек при старте
   initialPlaces.forEach(function (element) {
     const placeElement = placeTemplate.cloneNode(true);
   
     placeElement.querySelector('.place__name').textContent = element.name;
     placeElement.querySelector('.place__image').alt = element.name;
     placeElement.querySelector('.place__image').src = element.link;
+    setEventListeners(placeElement);
   
     placesList.append(placeElement);
   
   });
 
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-function openPopup(event) {
-    event.preventDefault()
-    popup.classList.add('popup_opened')
-    nameInput.value = profileName.textContent;
-    aboutInput.value = profileAbout.textContent;
+// открытие попапов общая
+const openModalWindow = (popup) => {
+    popup.classList.add('popup_opened');
+ }
+// открытие попапа профиля и заполнение полей
+const handleEditProfile = () => {
+   openModalWindow(editProfileModal);
+   nameInput.value = profileName.textContent;
+   aboutInput.value = profileAbout.textContent;
 }
 
-function closePopup(event) {
-    event.preventDefault()
-    popup.classList.remove('popup_opened')
+editButton.addEventListener('click', handleEditProfile);
+addButton.addEventListener('click', () => openModalWindow(addPlaceModal));
+
+// закрытие попапов
+const closePopup = (event) => {
+    event.preventDefault();
+    addPlaceModal.classList.remove('popup_opened');
+    editProfileModal.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (event) {
+closeButtonPlace.addEventListener('click', closePopup);
+closeButtonProfile.addEventListener('click', closePopup);
+
+// сохранение профиля
+function saveProfileHandler (event) {
     event.preventDefault(); 
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
     closePopup(event);
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+saveProfileButton.addEventListener('click', saveProfileHandler);
+
+// добавление разметки карточки
+function addCard(cardName, cardLink) {
+    const placeElement = placeTemplate.cloneNode(true);
+    
+    placeElement.querySelector('.place__name').textContent = cardName;
+    placeElement.querySelector('.place__image').alt = cardName;
+    placeElement.querySelector('.place__image').src = cardLink;
+    //сет
+    setEventListeners(placeElement);
+  
+    placesList.prepend(placeElement);
+  }
+// сохранение карточки
+saveCardButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const cardName = document.querySelector('#card-name');
+    const cardLink = document.querySelector('.popup__input_type_link');
+  
+    addCard(cardName.value, cardLink.value);
+
+    cardName.value = '';
+    cardLink.value = '';
+    closePopup(event);
+  });
+// удаление карточки 
+function handleDelete(evt) {
+	evt.target.closest('.place').remove();
+}
+
+//функция лайка 
+function handleLike(evt) {
+  evt.target.classList.toggle('place__like-button_active');
+}
+
+//set events 
+function setEventListeners (placeElement) {
+    const deleteCardButton = placeElement.querySelector('.place__delete-button')
+    deleteCardButton.addEventListener('click', handleDelete);
+    const likeCardButton = placeElement.querySelector('.place__like-button');
+    likeCardButton.addEventListener('click', handleLike);
+
+}
